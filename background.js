9685e590969cd1d272c760129b130e9584e09652
Calvin Wu <calvinwu4@gmail.com>
2020-10-27 23:07:24 -0400

    Permissions adding works

--- background.js
+++ background.js
@@ -4,7 +4,6 @@ chrome.runtime.onMessage.addListener(
         xhr.open("GET", request.url, true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
-                console.log(xhr.responseText);
                 sendResponse({ JSONresponse: JSON.parse(xhr.responseText) });
             }
         }
@@ -12,3 +11,68 @@ chrome.runtime.onMessage.addListener(
 
         return true;
     });
+
+
+
+function requestHostPermission(url){
+    chrome.permissions.request({
+        permissions: ['activeTab'],
+        origins: [url + "*"]
+        }, function(granted) {
+        // The callback argument will be true if the user granted the permissions.
+        if (!granted) {
+        if(chrome.runtime.lastError) console.error(chrome.runtime.lastError);
+        throw Error("Permission denied for host ");
+        }
+    });
+}
+
+urls = ['https://tigercenter.rit.edu/tigerCenterApp/api/class-search', 'https://www.reddit.com/'];
+
+chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+    if (info.status === 'complete') {
+        urls.forEach(url => {
+            if(new URL(url).hostname === new URL(tab.url).hostname && 
+            new URL(url).pathname === new URL(tab.url).pathname) {
+                chrome.tabs.insertCSS({
+                    file: "tooltipster/dist/css/tooltipster.main.min.css"
+                });
+                chrome.tabs.insertCSS({
+                    file: "tooltip.css"
+                });
+                chrome.tabs.executeScript({
+                    file: "jquery-3.3.1.min.js"
+                });
+                chrome.tabs.executeScript({
+                    file: "node_modules/papaparse/papaparse.min.js"
+                });
+                chrome.tabs.executeScript({
+                    file: "names.js"
+                });
+                chrome.tabs.executeScript({
+                    file: "addedNicknames.js"
+                });
+                chrome.tabs.executeScript({
+                    file: "arrive.min.js"
+                });
+                chrome.tabs.executeScript({
+                    file: "tooltipster/dist/js/tooltipster.bundle.min.js"
+                });
+                chrome.tabs.executeScript({
+                    file: "contentscript.js"
+                });
+            }
+        });
+    }
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    urls.forEach(url => {
+        if(new URL(url).hostname === new URL(tab.url).hostname && 
+        new URL(url).pathname === new URL(tab.url).pathname) {
+            chrome.tabs.executeScript({
+                code: requestHostPermission(tab.url),
+            });
+        }
+    });
+});
