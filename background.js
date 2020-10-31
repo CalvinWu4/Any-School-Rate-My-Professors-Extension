165b1e8e0a79367600a5b289dba9653f3b9f95fc
Calvin Wu <calvinwu4@gmail.com>
2020-10-30 23:41:36 -0400

    Use url data from airtable

--- background.js
+++ background.js
@@ -14,25 +14,51 @@ chrome.runtime.onMessage.addListener(
 
 function requestHostPermission(tab){
     chrome.permissions.request({
-        permissions: ['activeTab'],
-        origins: [tab.url + "*"]
-        }, function(granted) {
-            var code = 'window.location.reload();';
-            chrome.tabs.executeScript(tab.id, {code: code});          
-            // The callback argument will be true if the user granted the permissions.
-            if (!granted) {
+            origins: [tab.url + "*"]
+        }, function(granted) { // The callback argument will be true if the user granted the permissions.
+            if (granted) {
+                // Refresh the page to inject the code
+                var code = 'window.location.reload();';
+                chrome.tabs.executeScript(tab.id, {code: code});          
+            }
+            else {
                 if(chrome.runtime.lastError) console.error(chrome.runtime.lastError);
                 throw Error("Permission denied for host ");
             }
     });
 }
 
-urls = ['https://tigercenter.rit.edu/tigerCenterApp/api/class-search', 'https://www.reddit.com/'];
+
+let savedUrls = JSON.parse(localStorage.getItem("urls"));
+
+// Sync saved data with airtable data
+fetch('https://airtable.calvinwu4.workers.dev/')
+    .then(response => response.json())
+    .then(data => { 
+        let urls = [];
+        const records = data.records;
+        records.forEach(function(record){
+            const fields = record.fields;
+            const url = fields.URL;
+            const selector = fields.Selector;
+            if (url && selector) {
+                urls.push(url);
+                // selectors.push(selector);
+            }
+        })
+        if (JSON.stringify(savedUrls) != JSON.stringify(urls)) {
+            localStorage.clear();
+            localStorage.setItem("urls", JSON.stringify(urls));
+            chrome.runtime.reload();
+        }
+    });
+
+// Rules for when to show page action and set icon color to active
 let conditions = [];
-urls.forEach(url => 
+savedUrls.forEach(url => 
     conditions.push(
         new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: new URL(url).hostname }
+        pageUrl: { hostEquals: new URL(url).hostname, pathEquals: new URL(url).pathname }
       })
 ))
 
@@ -47,8 +73,7 @@ chrome.runtime.onInstalled.addListener(function(details) {
     });
 });
   
-
-function insertContentScript(){
+function injectCode() {
     chrome.tabs.insertCSS({
         file: "tooltipster/dist/css/tooltipster.main.min.css"
     });
@@ -78,12 +103,14 @@ function insertContentScript(){
     });
 }
 
-chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
-    if (info.status === 'complete') {
-        insertContentScript();
-    }
-});
-
+// Give host permission to allow for injection of code
 chrome.pageAction.onClicked.addListener(function(tab) {
     requestHostPermission(tab)
+});
+
+// Inject code to page if host permission is granted
+chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+    if (info.status === 'complete') {
+        injectCode();
+    }
 });
